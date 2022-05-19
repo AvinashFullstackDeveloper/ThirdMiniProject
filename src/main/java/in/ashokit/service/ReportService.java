d3df915d32f5e1b7f275f25e9b57f6c5ae66c115
avinash gupta <avinashgupta@avinashs-MacBook-Air.local>
2022-05-19 08:05:03 +0530

    Initial commit setup for Third Java Mini Project

---
+++ src/main/java/in/ashokit/service/ReportService.java
@@ -0,0 +1,17 @@
+package in.ashokit.service;
+
+import java.util.List;
+
+import in.ashokit.request.SearchRequest;
+import in.ashokit.response.SearchResponse;
+
+public interface ReportService {
+	
+	public List<String> getPlanNames();
+	
+	public List<String> getPlanStatuses();
+	
+	public List<SearchResponse> searchPlans(SearchRequest request);
+
+	
+}
